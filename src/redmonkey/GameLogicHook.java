7b132e8f0c3ce575ee898f2e596fe94e685889c3
Pesegato <pesegato@outlook.com>
2015-03-11 12:13:28 +0100

    Added hook for game logic

---
+++ src/redmonkey/GameLogicHook.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package redmonkey;
+
+import com.badlogic.gdx.ai.btree.LeafTask;
+
+/**
+ *
+ */
+public interface GameLogicHook {
+    public void endedTask(LeafTask o);
+}

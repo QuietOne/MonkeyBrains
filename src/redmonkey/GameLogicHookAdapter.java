6c900c66a03ad75c56c0c2655611ff7075181496
Pesegato <pesegato@outlook.com>
2015-03-20 16:48:14 +0100

    Added GameLogicHookAdapter

---
+++ src/redmonkey/GameLogicHookAdapter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redmonkey;
+
+import com.badlogic.gdx.ai.btree.LeafTask;
+
+/**
+ *
+ */
+public class GameLogicHookAdapter implements GameLogicHook {
+
+    @Override
+    public void endedTask(LeafTask task, RMItem... items) {
+    }
+}
